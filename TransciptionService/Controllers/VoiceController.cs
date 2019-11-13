b42a5875fe40ca24b146aae821ee57faa45c6431
slorello89 <slorello89@gmail.com>
2019-11-13 16:01:59 -0500

    first commit

---
+++ TransciptionService/Controllers/VoiceController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Nexmo.Api.Voice.Nccos;
+using Nexmo.Api.Voice.Nccos.Endpoints;
+using System.Collections.Generic;
+using System.Net;
+
+namespace TransciptionService.Controllers
+{
+    public class VoiceController : Controller
+    {
+        const string BASE_URL = "BASE_URL";
+        const string LANGUAGE = "en-US";
+        public IConfiguration _configuration;
+        public VoiceController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public HttpStatusCode Events() {
+            return HttpStatusCode.OK;
+        }
+
+        [HttpGet]
+        public string Answer()
+        {
+            var webSocketAction = new ConnectAction()
+            {
+                Endpoint = new[]
+                {
+                    new WebsocketEndpoint()
+                    {
+                        Uri = $"wss://{BASE_URL}/ws",                        
+                        ContentType="audio/l16;rate=8000", 
+                        Headers = new Dictionary<string, string>(){{"language", LANGUAGE } }
+                        
+                    }
+                }
+            };
+
+            var ncco = new Ncco(webSocketAction);
+            return ncco.ToString();
+        }
+    }
+}
---
+++ TransciptionService/TranscriptionEngine.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TransciptionService
+{
+    public class TranscriptionEngine
+    {
+        const int SAMPLES_PER_SECOND = 8000;
+        const int BITS_PER_SAMPLE = 16;
+        const int NUMBER_OF_CHANNELS = 1;
+
+        static SpeechConfig _config = SpeechConfig.FromSubscription("SUBSCRIPTION_KEY", "REGION");
+        static PushAudioInputStream _inputStream = AudioInputStream.CreatePushStream(AudioStreamFormat.GetWaveFormatPCM(SAMPLES_PER_SECOND, BITS_PER_SAMPLE, NUMBER_OF_CHANNELS));
+        static AudioConfig _audioInput = AudioConfig.FromStreamInput(_inputStream);
+        static SpeechRecognizer _recognizer;
+        static bool _started = false;
+
+        public static async Task StartSpeechTranscriptionEngine(string language)
+        {
+            if (!_started)
+            {                
+                _config.SpeechRecognitionLanguage = language;
+                 _recognizer = new SpeechRecognizer(_config, _audioInput);
+                _recognizer.Recognized += _recognizer_Recognized;
+                await _recognizer.StartContinuousRecognitionAsync();
+            }
+            _started = true;
+        }
+
+        public static async void StopTranscriptionEngine()
+        {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            if (_started)
+            {
+                _recognizer.Recognized -= _recognizer_Recognized;
+                await _recognizer.StopContinuousRecognitionAsync();
+                _recognizer.Dispose();
+            }
+            _started = false;
+            sw.Stop();
+
+            Debug.WriteLine("Elapsed={0}", sw.Elapsed);
+        }
+
+        private static void _recognizer_Recognized(object sender, SpeechRecognitionEventArgs e)
+        {
+            Debug.WriteLine("Recognized: " + e.Result.Text);
+        }
+
+        public static async Task WriteToTranscriber(HttpContext context, WebSocket webSocket)
+        {
+            const int BUFFER_SIZE = 160 * 2;
+            var buffer = new byte[BUFFER_SIZE];            
+            
+            try
+            {
+                var init_buffer = new byte[500];
+                var initial_response = await webSocket.ReceiveAsync(new ArraySegment<byte>(init_buffer), CancellationToken.None);
+                var converted = Encoding.UTF8.GetString(init_buffer);                
+                var initial_object = JObject.Parse(converted);
+                string language;
+                if (initial_object.ContainsKey("language"))
+                {
+                    language = (string)initial_object["language"];
+                }
+                else
+                {
+                    language = "en-US";
+                }
+
+                await StartSpeechTranscriptionEngine(language);
+                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                while (!result.CloseStatus.HasValue)
+                {
+                    await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                    _inputStream.Write(buffer);
+
+                    Console.WriteLine(result.Count);
+                }
+                await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+            finally
+            {
+                StopTranscriptionEngine();
+            }
+        }
+    }
+}
